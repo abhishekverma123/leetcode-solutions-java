69aa663fc4b988acc2c6db4be6b8b763c2e45a33
ABHISHEK  VERMA <76154794+abhishekverma123@users.noreply.github.com>
2026-05-02 00:55:42 +0530

    Update maximumSubarray.java

--- arrays/maximumSubarray.java
+++ arrays/maximumSubarray.java
@@ -1,8 +1,18 @@
 Problem : Leetcode 53 Maximum SubArray
 Platform : Leetcode  
 Approach : Use KADANE Algorithm
+   Maintain a current sum (currentSum)
+- Add each element to currentSum
+- If currentSum becomes negative, reset it to 0
+- Track the maximum sum found so far
+  
+  Example :  Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+  Output: 6
+  Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+  
 Time Complexity : o(n)
 Space Complexity : o(1)  
+  
   
 Code :
 
@@ -25,4 +35,3 @@ Code :
 }
 
 
-  
