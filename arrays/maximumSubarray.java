ec652c42ce0d1fb781921596f6cd1fd6b6763983
ABHISHEK  VERMA <76154794+abhishekverma123@users.noreply.github.com>
2026-05-02 00:51:08 +0530

    Update maximumSubarray.java

--- arrays/maximumSubarray.java
+++ arrays/maximumSubarray.java
@@ -1,1 +1,28 @@
+Problem : Leetcode 53 Maximum SubArray
+Platform : Leetcode  
+Approach : Use KADANE Algorithm
+Time Complexity : o(n)
+Space Complexity : o(1)  
+  
+Code :
 
+  class Solution {
+    public int maxSubArray(int[] nums) {
+      int max = Integer.MIN_VALUE, cs = 0;
+      for(int i=0;i<nums.length;i++){
+        cs = cs + nums[i];
+        if(cs > max){
+            max = cs;
+        }
+        if(cs < 0){
+            cs = 0;
+        }
+      }
+      return max;
+
+        
+    }
+}
+
+
+  
